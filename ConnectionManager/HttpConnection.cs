fe502462e270e7c1f89eb0d664466360c79ed74a
Samir Reyes <samir1604@gmail.com>
2026-01-08 15:23:21 -0500

    ref(HttpConnection):Refactored get and post connection method

--- ConnectionManager/HttpConnection.cs
+++ ConnectionManager/HttpConnection.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,10 @@ namespace ConnectionManager
 {
     public class HttpConnection
     {
-        private readonly HttpClient _httpClient;
-        private readonly CookieContainer _cookieContainer;
+        private static HttpClient _httpClient;
+        private static CookieContainer _cookieContainer;
 
-        public HttpConnection() 
+        static HttpConnection() 
         {
             _cookieContainer = new CookieContainer();
             var handler = new HttpClientHandler
@@ -28,62 +29,79 @@ namespace ConnectionManager
             _httpClient = new HttpClient(handler);
         }
 
-        public async Task<HttpResponse> Get(string url, Dictionary<string, string>? customHeaders = null)
+        public async Task<HttpResponse> Get(string url, Action<HttpRequestHeaders>? configureHeaders = null)
         {
             try
             {
                 using var request = new HttpRequestMessage(HttpMethod.Get, url);
-                if(customHeaders != null)                
-                    foreach(var header in customHeaders)                    
-                        request.Headers.TryAddWithoutValidation(header.Key, header.Value);                    
-
-                var response = await _httpClient.SendAsync(request);                
-                response.EnsureSuccessStatusCode();
-
-                string content = await response.Content.ReadAsStringAsync();
-
-                var fields = response.Content.Headers.ContentType?.MediaType == "text/html" 
-                    ? ParseHtmlFields(content) : [];
-                                
-                var cookies = _cookieContainer.GetCookies(new Uri(url))
-                    .Cast<Cookie>().ToDictionary(n => n.Name, v => v.Value);
-
-                var headers = response.Headers.ToDictionary(k => k.Key, v => v.Value.ToArray());
-
-                return new HttpResponse
-                {
-                    Status = (int)response.StatusCode,
-                    Response = content,
-                    UrlRedirect = response.RequestMessage?.RequestUri?.ToString() ?? url,
-                    Headers = headers,
-                    Cookies = cookies,
-                    FormFields = fields                    
-                };
-            }
-            catch(HttpRequestException ex)
-            {
-                return new HttpResponse
-                {
-                    Status = ex.StatusCode != null ? (int)ex.StatusCode : 500,
-                    Message = ex.Message,                    
-                };
-            }
-            catch(TaskCanceledException ex)
-            {
-                return new HttpResponse
-                {
-                    Status = (int)HttpStatusCode.RequestTimeout,
-                    Message = ex.Message,
-                };
-            }
+                configureHeaders?.Invoke(request.Headers);
+                
+                var response = await _httpClient.SendAsync(request);
+                return await ProcessResponse(response, url);
+            }            
             catch (Exception ex)
             {
-                return new HttpResponse
-                {
-                    Status = 500,
-                    Message = ex.Message,
-                };
+                return HandleException(ex);
             }
+        }        
+
+        public async Task<HttpResponse> Post(
+            string url, Dictionary<string, string> data, Action<HttpRequestMessage>? configureHeaders = null)
+        {
+            try
+            {                
+                using var request = new HttpRequestMessage(HttpMethod.Post, url);                
+                request.Content = new FormUrlEncodedContent(data);
+                configureHeaders?.Invoke(request);                
+
+                var response = await _httpClient.SendAsync(request);
+                return await ProcessResponse(response, url);                
+            }            
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        private HttpResponse HandleException(Exception ex)
+        {
+            int status = ex switch
+            {
+                HttpRequestException hrex => hrex.StatusCode != null ? (int)hrex.StatusCode : 500,
+                TaskCanceledException => (int)HttpStatusCode.RequestTimeout,
+                _ => 500
+            };
+
+            return new HttpResponse { Status = status, Message = ex.Message };
+        }
+
+        private async Task<HttpResponse> ProcessResponse(
+            HttpResponseMessage response, string originalUrl)
+        {
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            var fields = response.Content.Headers.ContentType?.MediaType == "text/html"
+                ? ParseHtmlFields(content) : [];
+
+            var cookies = _cookieContainer
+                .GetCookies(new Uri(originalUrl))
+                .Cast<Cookie>()
+                .ToDictionary(n => n.Name, v => v.Value);
+
+            var headers = response.Headers
+                .ToDictionary(k => k.Key, v => v.Value.ToArray());
+
+            return new HttpResponse
+            {
+                Status = (int)response.StatusCode,
+                Response = content,
+                UrlRedirect = response.RequestMessage?.RequestUri?.ToString() ?? originalUrl,
+                Headers = headers,
+                Cookies = cookies,
+                FormFields = fields
+            };
         }
 
         private Dictionary<string, string> ParseHtmlFields(string content)
@@ -94,9 +112,9 @@ namespace ConnectionManager
             htmlDoc.LoadHtml(content);
 
             var nodes = htmlDoc.DocumentNode.SelectNodes($"//input[@name]");
-            if(nodes != null)
+            if (nodes != null)
             {
-                foreach(var node in nodes)
+                foreach (var node in nodes)
                 {
                     string name = node.GetAttributeValue("name", "");
                     string value = node.GetAttributeValue("value", "");
@@ -106,55 +124,6 @@ namespace ConnectionManager
             }
 
             return fields;
-        }
-
-        public async Task<HttpResponse> Post(string url, Dictionary<string, string>? customHeaders, Dictionary<string, string> data)
-        {
-            try
-            {
-                var content = new FormUrlEncodedContent(data);
-                using var request = new HttpRequestMessage(HttpMethod.Post, url);                
-
-                if (customHeaders != null)
-                    foreach (var header in customHeaders)
-                        request.Headers.TryAddWithoutValidation(header.Key, header.Value);
-
-                var response = await _httpClient.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-
-                string body = await response.Content.ReadAsStringAsync();
-
-                return new HttpResponse
-                {
-                    Status = (int)response.StatusCode,
-                    Response = body,
-                    UrlRedirect = response.RequestMessage?.RequestUri?.ToString() ?? url,                    
-                };
-            }
-            catch (HttpRequestException ex)
-            {
-                return new HttpResponse
-                {
-                    Status = ex.StatusCode != null ? (int)ex.StatusCode : 500,
-                    Message = ex.Message,
-                };
-            }
-            catch (TaskCanceledException ex)
-            {
-                return new HttpResponse
-                {
-                    Status = (int)HttpStatusCode.RequestTimeout,
-                    Message = ex.Message,
-                };
-            }
-            catch (Exception ex)
-            {
-                return new HttpResponse
-                {
-                    Status = 500,
-                    Message = ex.Message,
-                };
-            }
         }
     }
 }
